88e19818145ad81b4cd187a2933e29b21de8b135
Jie Ren <chloe@Jies-MacBook-Air.local>
2021-11-17 23:31:39 -0800

    add profile service

---
+++ services/profile-service.js
@@ -0,0 +1,13 @@
+let profile = require('../data/profile.json');
+module.exports = (app) => {
+    const getCurrentProfile = (req, res) => {
+        res.json(profile);
+    }
+    app.get('/api/profile', getCurrentProfile);
+
+    const updateCurrentProfile = (req, res) => {
+        profile = req.body;
+        res.sendStatus(200);
+    }
+    app.put('/api/profile', updateCurrentProfile);
+};
